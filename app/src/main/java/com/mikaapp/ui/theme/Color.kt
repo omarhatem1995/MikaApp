c9f133e7e38c926cc0fa51e23e5541fc2a14fd9d
eslam <eslam.hamdi.hassan@gmail.com>
2022-11-14 00:22:17 +0200

    -adding splash screen

--- app/src/main/java/com/mikaapp/ui/theme/Color.kt
+++ app/src/main/java/com/mikaapp/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.mikaapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val MikaGreen = Color(0xFF008c3c)
+val MikaGreen500 = Color(0xFF005d10)
+val MikaGreen200 = Color(0xFF4dbd68)
+val Teal200 = Color(0xFFFF9800)
